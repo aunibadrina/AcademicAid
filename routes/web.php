bd2609b0a86b1f17c8a66ff68797553f79e8b6cc
Muhd Afnan <88524435+zeynanz@users.noreply.github.com>
2023-12-28 12:04:11 +0800

    web php update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
+Route::view('/','welcome');
+Route::view('login','login');
+Route::view('tutor','tutor');
+
