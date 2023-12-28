5fc9a62be6433aec4e4f4b8c17887866eb554b98
Muhd Afnan <88524435+zeynanz@users.noreply.github.com>
2023-12-28 13:13:07 +0800

    Update landing.blade.php

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,7 +4,7 @@ Result Size: 1450 x 337
 <!DOCTYPE html>
 <html>
 <head>
-<title>Academic Aid</title>
+<title>Academic Aidz</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -17,6 +17,21 @@ h1, h2, h3, h4, h5, h6 {
 </style>
 </head>
 <body>
+<style>
+a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: red;
+}
+</style>
+<a href="login" target="_blank">This is a link</a>
 ​
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
@@ -33,6 +48,7 @@ h1, h2, h3, h4, h5, h6 {
 ​
 <!-- Header -->
 <header class="w3-display-container w3-content w3-wide" style="max-width:1600px;min-width:500px" id="home">
+<a href="login" target="_blank">Login Page</a>
   <img class="w3-image" src="/w3images/hamburger.jpg" alt="Hamburger Catering" width="1600" height="800">
   <div class="w3-display-bottomleft w3-padding-large w3-opacity">
     <h1 class="w3-xxlarge">Forum</h1>
